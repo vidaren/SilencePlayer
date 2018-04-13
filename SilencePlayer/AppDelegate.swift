ccd2b7de58dc99ca4ad8a5af755678927082f057
vidar <vidar@vidartysse.net>
2018-04-13 19:14:22 +0200

    Initial Commit

---
+++ SilencePlayer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SilencePlayer
+//
+//  Created by Vidar on 13.04.2018.
+//  Copyright © 2018 Vidar Tysse. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SilencePlayer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SilencePlayer
+//
+//  Created by Vidar on 13.04.2018.
+//  Copyright © 2018 Vidar Tysse. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
