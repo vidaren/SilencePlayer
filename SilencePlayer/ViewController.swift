3fa251a1a3d86a93095493e342a4ee14469cd87d
vidar <vidar@vidartysse.net>
2018-05-18 21:48:06 +0200

    Added checkbox to play silence on startup.

--- SilencePlayer/ViewController.swift
+++ SilencePlayer/ViewController.swift
@@ -13,14 +13,27 @@ var soundPlayer: SoundPlayer?
 class ViewController: NSViewController {
     
     @IBOutlet weak var nowPlayingLabel: NSTextField!
-
+    @IBOutlet weak var PlayOnStartupCheckbox: NSButton!
+    @IBOutlet weak var PlayButton: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        PlayOnStartupCheckbox.state = UserDefaults.standard.bool(forKey: "PlayOnStartup")
+            ? NSControl.StateValue.on
+            : NSControl.StateValue.off;
+        if (PlayOnStartupCheckbox.state == NSControl.StateValue.on) {
+            playButtonClicked(PlayButton);
+        }
     }
 
     override var representedObject: Any? {
         didSet {
         }
+    }
+    
+    @IBAction func playOnStartupChanged(_ sender: NSButton) {
+        let isChecked : Bool = PlayOnStartupCheckbox.state == NSControl.StateValue.on;
+        UserDefaults.standard.set(isChecked, forKey: "PlayOnStartup");
     }
 
     @IBAction func playButtonClicked(_ sender: Any) {
